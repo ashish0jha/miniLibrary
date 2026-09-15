e2158cfd55867ad56249074b3d71baef5df31b93
ashish0jha <ashish2893jnv@gmail.com>
2026-09-15 15:27:43 +0530

    Added Summary api for admin

--- server/src/controller/adminOnly.ts
+++ server/src/controller/adminOnly.ts
@@ -16,3 +16,28 @@ export async function overdueHandler(req:Request,res:Response) {
         res.status(400).json({error:(err as Error).message})
     }
 }
+
+export async function summaryHandler(req:Request,res:Response) {
+    try{
+        const query = `SELECT 
+        ROW_NUMBER() OVER(ORDER BY b.id DESC) AS serial_no,
+        b.id AS book_id,
+        b.title AS book_title,
+        COUNT(br.book_id) AS no_of_borrowed_book,
+        ARRAY_AGG(u.username) AS borrow_by 
+        FROM borrow_records br JOIN books b ON br.book_id = b.id 
+        JOIN users u ON u.id = br.user_id 
+        WHERE br.returned_date IS NULL
+        GROUP BY b.id , b.title
+        ORDER BY no_of_borrowed_book DESC`;
+
+        const result = await pgClinet.query(query);
+        if(result.rows.length === 0) {
+            throw new Error("No Books for the Summary");
+        }
+        res.json({msg:"Summary fetched Successful",data:result.rows});
+    }
+    catch(err) {
+        res.status(400).json({error:(err as Error).message})
+    }
+}
--- server/src/routes/adminOnly.ts
+++ server/src/routes/adminOnly.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { requireRole, userAuth } from '../middleware/auth.js';
-import { overdueHandler } from '../controller/adminOnly.js';
+import { overdueHandler, summaryHandler } from '../controller/adminOnly.js';
 
 const router = express.Router();
 
 router.get('/overdue',userAuth,requireRole('admin'),overdueHandler);
+router.get('/summary',userAuth,requireRole('admin'),summaryHandler);
 
 export default router;
