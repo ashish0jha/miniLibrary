64e269a236615b8331ff8b03498b6c16dc5427d1
ashish0jha <ashish2893jnv@gmail.com>
2026-09-15 14:21:52 +0530

    Updated the leaderboard ui with rank column and added the borrowed by book

--- server/src/controller/library.ts
+++ server/src/controller/library.ts
@@ -3,7 +3,7 @@ import pgClinet from "../config/db.js";
 
 export async function leaderboardHandler(req: Request, res: Response) {
   try {
-    const query = `SELECT b.id, b.title,b.author,COUNT(bb.book_id) AS borrow_count FROM borrow_records bb RIGHT JOIN books b ON bb.book_id = b.id GROUP BY b.id, b.title,b.author ORDER BY borrow_count DESC`;
+    const query = `SELECT DENSE_RANK() OVER(ORDER BY COUNT(bb.book_id) DESC) AS rank, b.id, b.title,b.author,COUNT(bb.book_id) AS borrow_count FROM borrow_records bb RIGHT JOIN books b ON bb.book_id = b.id GROUP BY b.id, b.title,b.author ORDER BY borrow_count DESC`;
 
     const result = await pgClinet.query(query);
 
@@ -11,4 +11,20 @@ export async function leaderboardHandler(req: Request, res: Response) {
   } catch (err) {
     res.status(500).json({error:(err as Error).message});
   }
+}
+
+export async function totalBorrowesBystudentHandler(req:Request,res:Response) {
+    try{
+        const query = `SELECT b.title,b.author,bb.borrowed_at FROM borrow_records bb JOIN books b ON bb.book_id = b.id WHERE bb.user_id = $1 AND returned_date IS NULL`;
+
+        const result = await pgClinet.query(query,[req.user?.id]);
+        if(result.rows.length === 0) {
+            throw new Error("No Books Borrowed Yet");
+        }
+
+        res.json({msg:"fetch Successful",data:result.rows});
+    }
+    catch(err) {
+        res.status(500).json({error:(err as Error).message});
+    }
 }
--- server/src/routes/library.ts
+++ server/src/routes/library.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { userAuth } from '../middleware/auth.js';
-import { leaderboardHandler } from '../controller/library.js';
+import { leaderboardHandler, totalBorrowesBystudentHandler } from '../controller/library.js';
 
 const router = express.Router();
 
 router.get("/leaderboard",userAuth,leaderboardHandler);
+router.get("/borrowed",userAuth,totalBorrowesBystudentHandler)
 
 export default router;
